1820bab9a019004efa6325e8ffe099ee719ab8ce
MarkusHackspacher <hackspacher@gmx.de>
2014-07-13 18:27:51 +0200

    pep8 changes

--- miner/menus.py
+++ miner/menus.py
@@ -4,9 +4,11 @@ import sys
 from miner.data import filepath
 from miner.constants import *
 
-# menu object
+
 class MenuClass():
     def __init__(self, surface):
+        '''menu object
+        '''
         self.surface = surface
 
         self.backgroundColor = (0, 0, 0)
@@ -66,7 +68,8 @@ class MenuClass():
 
             self._updateSelectedMenu()
 
-        if pressed(pygame.K_SPACE) or pressed(pygame.K_RETURN) or pressed(pygame.K_x):
+        if pressed(pygame.K_SPACE) or pressed(pygame.K_RETURN) or pressed(
+                pygame.K_x):
             if len(self.menus) != 0:
                 self.menus[self.selectedMenu].click()
 
@@ -74,14 +77,15 @@ class MenuClass():
             pygame.quit()
             sys.exit()
 
-    def addMenu(self, caption, fgColor=(255,255,255)):
+    def addMenu(self, caption, fgColor=(255, 255, 255)):
         self.menus.append(MenuItemClass(caption, fgColor=fgColor))
 
         # update placement (center)
         menuAmount = len(self.menus)
         for i in range(len(self.menus)):
             self.menus[i].rect.centerx = SCREEN_WIDTH / 2
-            self.menus[i].rect.centery = SCREEN_HEIGHT / (menuAmount + 1) * (i+1)
+            self.menus[i].rect.centery = SCREEN_HEIGHT / (
+                menuAmount + 1) * (i + 1)
 
         self._updateSelectedMenu()
 
@@ -94,7 +98,7 @@ class MenuClass():
 
 
 class MenuItemClass(pygame.sprite.Sprite):
-    def __init__(self, caption='', antialias=0, fgColor=(255,255,255)):
+    def __init__(self, caption='', antialias=0, fgColor=(255, 255, 255)):
         pygame.sprite.Sprite.__init__(self)
 
         self.caption = caption
@@ -112,21 +116,25 @@ class MenuItemClass(pygame.sprite.Sprite):
         surface.blit(self.image, self.rect)
 
     def select(self):
-        self.image = self.font.render(self.caption, self.antialias, (100, 100, 100))
+        self.image = self.font.render(
+            self.caption, self.antialias, (100, 100, 100))
 
     def deselect(self):
-        self.image = self.font.render(self.caption, self.antialias, self.fgColor)
+        self.image = self.font.render(
+            self.caption, self.antialias, self.fgColor)
 
     def click(self):
-        if self.event != None:
+        if self.event is not None:
             self.event()
 
     def connect(self, action):
         self.event = action
 
-# menus
+
 class MenuStartGame(MenuClass):
     def __init__(self, surface, engine):
+        '''menus
+        '''
         MenuClass.__init__(self, surface)
 
         self.engine = engine
@@ -151,7 +159,6 @@ class MenuStartGame(MenuClass):
         sys.exit()
 
 
-
 class MenuScene(MenuClass):
     def __init__(self, surface, engine):
         MenuClass.__init__(self, surface)
@@ -160,22 +167,44 @@ class MenuScene(MenuClass):
 
         self.currentScene = 0
 
-        self.scenes = ['Good morning Sam.\nHow are you feeling today?', \
-        'Your final mission before going home to Earth,\n is to gather the requested RESOURCES.\n\nI sincerely hope your enjoy your last week and\nI hope life on Earth is exactly how you remember it to be.', \
-        'You will have a 5% chance of finding RESOURCES in a regular moon rock,\nand a 100% chance in rich moon rocks.\n\nOur scans tell us that the deeper you dig,\nthe more resource rich rocks you will locate.\n\nYou should spend your time wisely.', \
-        '... and remember Sam.\nYou must reach an ALTITUDE of above 0 afterwards\nor we will not be able to pick you up.',  \
-        'CONTROLS:\nArrow keys for movement and aiming\nX for jumping\nC for destroying blocks\nV for creating blocks (requires 4 RESOURCES)', \
-        \
-        'Well done. I am sure your family back home on Earth is proud of you.\nAre you excited to go back to them?\n\nSam, get some sleep. You are very tired.', \
-        \
-        'You are really close to going back now.\nAre you not happy to hear that?',
-        \
-        'I am going to miss you Sam. I want you to know that.\nMaybe the next one will be like you.\n\nWe will see...',
-        \
-        'Sam.... you do not look too well. How are you feeling?\n\nAre you feeling okay?',
-        \
-        'Sam... Sam...\nCan you hear me? Sam?\n\n*Contacting the company*\nSam #073 is dead.\nRequesting the assistance of Sam #074 ASAP.']
+        self.scenes = ['Good morning Sam.\nHow are you feeling today?',
 
+        'Your final mission before going home to Earth,\n'
+        'is to gather the requested RESOURCES.\n\nI sincerely hope your'
+        ' enjoy your last week and\nI hope life on Earth is exactly'
+        ' how you remember it to be.',
+
+        'You will have a 5% chance of finding RESOURCES in a '
+        'regular moon rock,\nand a 100% chance in rich moon rocks.\n\n'
+        'Our scans tell us that the deeper you dig,\n'
+        'the more resource rich rocks you will locate.\n\n'
+        'You should spend your time wisely.',
+
+        '... and remember Sam.\n'
+        'You must reach an ALTITUDE of above 0 afterwards\n'
+        'or we will not be able to pick you up.',
+
+        'CONTROLS:\nArrow keys for movement and aiming\n'
+        'X for jumping\n'
+        'C for destroying blocks\n'
+        'V for creating blocks (requires 4 RESOURCES)',
+
+        'Well done. I am sure your family back home on '
+        'Earth is proud of you.\n'
+        'Are you excited to go back to them?'
+        '\nSam, get some sleep. You are very tired.',
+
+        'You are really close to going back now.\n'
+        'Are you not happy to hear that?',
+
+        'I am going to miss you Sam. I want you to know that.\n'
+        'Maybe the next one will be like you.\n\nWe will see...',
+
+        'Sam.... you do not look too well. How are you feeling?\n'
+        '\nAre you feeling okay?',
+
+        'Sam... Sam...\nCan you hear me? Sam?\n\n*Contacting the company*\n'
+        'Sam #073 is dead.\nRequesting the assistance of Sam #074 ASAP.']
 
     def draw(self):
         self.surface.fill(self.backgroundColor)
@@ -218,16 +247,19 @@ class MenuScene(MenuClass):
 
             textRect = textSurface.get_rect()
             textRect.centerx = SCREEN_WIDTH / 2
-            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(texts[i])[1]
+            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(
+                texts[i])[1]
 
             self.surface.blit(textSurface, textRect)
 
     def drawHint(self):
-        textSurface = self.textFont.render('(Click X to continue)', 0, (255, 255, 255))
+        textSurface = self.textFont.render(
+            '(Click X to continue)', 0, (255, 255, 255))
         textRect = textSurface.get_rect()
-        textRect.centerx = SCREEN_WIDTH/2
+        textRect.centerx = SCREEN_WIDTH / 2
         textRect.centery = SCREEN_HEIGHT - 50
         self.surface.blit(textSurface, textRect)
+
 
 class MenuAbout(MenuClass):
     def __init__(self, surface, engine):
@@ -237,7 +269,8 @@ class MenuAbout(MenuClass):
 
         self.currentScene = 0
 
-        self.text = 'ABOUT\nMINER is a PyWeek entry by Marcus Moller.\nWritten in Python (2.7) and PyGame.\n\nmarcusmoller @ GitHub'
+        self.text = ('ABOUT\nMINER is a PyWeek entry by Marcus Moller.\n'
+            'Written in Python (2.7) and PyGame.\n\nmarcusmoller @ GitHub')
 
     def draw(self):
         self.surface.fill(self.backgroundColor)
@@ -270,16 +303,19 @@ class MenuAbout(MenuClass):
 
             textRect = textSurface.get_rect()
             textRect.centerx = SCREEN_WIDTH / 2
-            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(texts[i])[1]
+            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(
+                texts[i])[1]
 
             self.surface.blit(textSurface, textRect)
 
     def drawHint(self):
-        textSurface = self.textFont.render('(Click ESC to return)', 0, (255, 255, 255))
+        textSurface = self.textFont.render(
+            '(Click ESC to return)', 0, (255, 255, 255))
         textRect = textSurface.get_rect()
-        textRect.centerx = SCREEN_WIDTH/2
+        textRect.centerx = SCREEN_WIDTH / 2
         textRect.centery = SCREEN_HEIGHT - 50
         self.surface.blit(textSurface, textRect)
+
 
 class MenuGameOver(MenuClass):
     def __init__(self, surface, engine):
@@ -287,7 +323,8 @@ class MenuGameOver(MenuClass):
 
         self.engine = engine
 
-        self.text = '... Sam.\nYou failed.\n\nYou will never make it back to Earth now.'
+        self.text = ('... Sam.\n'
+        'You failed.\n\nYou will never make it back to Earth now.')
 
     def draw(self):
         self.surface.fill(self.backgroundColor)
@@ -320,16 +357,19 @@ class MenuGameOver(MenuClass):
 
             textRect = textSurface.get_rect()
             textRect.centerx = SCREEN_WIDTH / 2
-            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(texts[i])[1]
+            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(
+                texts[i])[1]
 
             self.surface.blit(textSurface, textRect)
 
     def drawHint(self):
-        textSurface = self.textFont.render('(Click X to return to main screen)', 0, (255, 255, 255))
+        textSurface = self.textFont.render(
+            '(Click X to return to main screen)', 0, (255, 255, 255))
         textRect = textSurface.get_rect()
-        textRect.centerx = SCREEN_WIDTH/2
+        textRect.centerx = SCREEN_WIDTH / 2
         textRect.centery = SCREEN_HEIGHT - 50
         self.surface.blit(textSurface, textRect)
+
 
 class MenuGameFinish(MenuClass):
     def __init__(self, surface, engine):
@@ -337,7 +377,8 @@ class MenuGameFinish(MenuClass):
 
         self.engine = engine
 
-        self.text = 'GAME COMPLETED\nThanks for playing MINER, my PyWeek #17 entry!'
+        self.text = ('GAME COMPLETED\n'
+            'Thanks for playing MINER, my PyWeek #17 entry!')
 
     def draw(self):
         self.surface.fill(self.backgroundColor)
@@ -370,13 +411,15 @@ class MenuGameFinish(MenuClass):
 
             textRect = textSurface.get_rect()
             textRect.centerx = SCREEN_WIDTH / 2
-            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(texts[i])[1]
+            textRect.centery = SCREEN_HEIGHT / 2 + i * self.textFont.size(
+                texts[i])[1]
 
             self.surface.blit(textSurface, textRect)
 
     def drawHint(self):
-        textSurface = self.textFont.render('(Click X to return to main screen)', 0, (255, 255, 255))
+        textSurface = self.textFont.render(
+            '(Click X to return to main screen)', 0, (255, 255, 255))
         textRect = textSurface.get_rect()
-        textRect.centerx = SCREEN_WIDTH/2
+        textRect.centerx = SCREEN_WIDTH / 2
         textRect.centery = SCREEN_HEIGHT - 50
         self.surface.blit(textSurface, textRect)
